db9455a34feb05f5b8db6b56fb42dbace18a9f1d
Norton Manfré <nortonmanfrejr@hotmail.com>
2025-02-14 02:10:41 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FaceAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
